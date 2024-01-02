abed295adcb8e7d6de57718a254eb80804e10c1f
flemay <flemay@gmail.com>
2024-01-02 21:47:39 +0100

    Désactiver le code permettant de cacher le lien vers le dashboard via une entourloupe CSS
    Cette fonctionnalité sera implémenté dans un plugin.

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -7,17 +7,20 @@
  * Pour se faire il faut utiliser l'approche du thème classique, c-à-d enregistrer
  * un nouveau menu, puis dans l'éditeur de site (FSE), sélectionner un menu classique.
  */
-function hide_admin_elements()
-{
-    // $is_not_logged_in = ! curent_user_can('administrator');
 
-    if (! is_user_logged_in()) {
-        echo '<style>';
-        echo '.planty-admin-visibilty {
-                display: none !important;
-              }';
-        echo '</style>';
-    }
-}
-
-add_filter('wp_head', 'hide_admin_elements');
+/*
+ * function hide_admin_elements()
+ * {
+ *     // $is_not_logged_in = ! curent_user_can('administrator');
+ *
+ *     if (! is_user_logged_in()) {
+ *         echo '<style>';
+ *         echo '.planty-admin-visibilty {
+ *                 display: none !important;
+ *               }';
+ *         echo '</style>';
+ *     }
+ * }
+ *
+ * add_filter('wp_head', 'hide_admin_elements');
+ */
