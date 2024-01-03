1d3ff96485346b2125707a9c90ad06fd7f899dfd
flemay <flemay@gmail.com>
2024-01-03 11:20:16 +0100

    Admin link :commenter functions.php et admin-link.php pour expliquer le développement de la fonctionnalité

--- wp-content/plugins/admin-link/admin-link.php
+++ wp-content/plugins/admin-link/admin-link.php
@@ -7,19 +7,37 @@
  * Author URI: http://flemay.com 
  */
 
+/* Version 6.1 (oct 2022) 
+ * "This update added a filter to navigation.php to provide the ability to 
+ *  modify navigation block menu items with code. Developers now can 
+ *  programmatically access the navigation block menu items of a block theme 
+ *  similar to the existing wp_nav_menu_items filter does for traditional 
+ *  themes.
+ *
+ *  A new filter, block_core_navigation_render_inner_blocks, passes 
+ *  WP_Block_List::$inner_blocks to the developer.
+ *  https://make.wordpress.org/core/2022/10/10/miscellaneous-editor-changes-for-wordpress-6-1/#navigation-block-menu-items-filter
+ */
+
 function hide_admin_link($inner_blocks)
 {
 
   if (!is_user_logged_in()) {
 
+    // WP_Block_List ($inner_blocks) is a itirable class. 
+    // Should use built-in methods? 
+    // Didn't play yet with PHP's OOP. 
+
     foreach ($inner_blocks as $key => $inner_block) {
 
+      // *** Trying to figure out what is going on here ***
       // echo var_dump($inner_block->parsed_block);
       
       // foreach($inner_block as $key => $block_content) {
       //     echo '<b>' . $key . ' (' . gettype($block_content) . ')'. ' : </b>';
       //     echo var_dump($block_content) . '<br><br>';
       // }
+      //
 
       $url = $inner_block->parsed_block['attrs']['url'];
 
--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,26 +1,41 @@
 <?php
 
 /*
- * Cache aux utilisateurs normaux les éléments liés au prévilège d'administrateur.
- * Le block theme ne permet pas par défaut d'effectuer cette modification via
- * le backend (en utilisant par exemple le filtre wp_nav_menu_items  ou _objects).
- * Pour se faire il faut utiliser l'approche du thème classique, c-à-d enregistrer
- * un nouveau menu, puis dans l'éditeur de site (FSE), sélectionner un menu classique.
+ * Cache, via une astuce CSS, aux utilisateurs non loggués les éléments choisis
+ * (classe .planty-admin-visibility).
+ *
+ * Cette fonctionnalité devrait être implémentée côté serveur. L'astuce du
+ * CSS ne fait que masquer l'item : il reste tout de même présent côté utilisa-
+ * teur.
+ *
+ * Le filtre wp_nav_menu_items permet de filtrer le contenu d'un menu avant qu'
+ * il soit transmis au client.
+ *
+ * Or il n'est pas possible d'utiliser directement ce hook avec un "block theme".
+ * Pour y arriver il faudrait enregistrer le menu (register_menu), ce qui
+ * transformerait de facto le "block theme" en "hybrid theme". En effet, cet
+ * enregistrement active le menu "Customizer" dans le dashboard, qui est une
+ * fonction propre aux thèmes classiques.
+ *
+ * Un nouveau hook a été créé dans la m-à-j 6.1 (octobre 2022) pour offrir
+ * aux développeurs de "block themes" la même flexibilité que le filtre
+ * wp_nav_menu_items offre pour les "classical themes".
+ *
+ * Ce filtre est utilisé par le plugin déveleppé pour le site Planty.
+ * (ref. plugin admin-link)
  */
 
-/*
- * function hide_admin_elements()
- * {
- *     // $is_not_logged_in = ! curent_user_can('administrator');
- *
- *     if (! is_user_logged_in()) {
- *         echo '<style>';
- *         echo '.planty-admin-visibilty {
- *                 display: none !important;
- *               }';
- *         echo '</style>';
- *     }
- * }
- *
- * add_filter('wp_head', 'hide_admin_elements');
- */
+// function hide_admin_elements()
+// {
+//     // $is_not_logged_in = ! curent_user_can('administrator');
+//
+//     if (! is_user_logged_in()) {
+//         echo '<style>';
+//         echo '.planty-admin-visibilty {
+//                   display: none !important;
+//                 }';
+//         echo '</style>';
+//     }
+// }
+//
+// add_filter('wp_head', 'hide_admin_elements');
