6c1a276c5fc9f572efca59ed4b32da347415376f
flemay <flemay@gmail.com>
2024-01-09 12:32:17 +0100

    Restorer le fichier functions.php qui a été accidentellement effacé

---
+++ wp-content/themes/planty/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Cache, via une astuce CSS, aux utilisateurs non loggués les éléments choisis
+ * (classe .planty-admin-visibility).
+ *
+ * Désactivé : finctionnalité remplacée par le plugin Hide admin link.
+ */
+
+// function hide_admin_elements()
+// {
+//     // $is_not_logged_in = ! curent_user_can('administrator');
+//
+//     if (! is_user_logged_in()) {
+//         echo '<style>';
+//         echo '.planty-admin-visibilty {
+//                   display: none !important;
+//                 }';
+//         echo '</style>';
+//     }
+// }
+//
+// add_filter('wp_head', 'hide_admin_elements');
